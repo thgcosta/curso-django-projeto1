028364d5158e29fa76fc0b26952890d6aa83da1e
Thiago Costa <thg.costa@icloud.com>
2023-02-02 16:23:03 -0300

    projeto iniciado nome Recipes

---
+++ recipes/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'recipes/home.html', context={
+        'name': 'Thiago Costa',
+    })
+
+
+def sobre(request):
+    return HttpResponse('Sobre')
+
+
+def contato(request):
+    return HttpResponse('contato')
