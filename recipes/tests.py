d4aeb751b2da564354d0b515ad27382868821017
Thiago Costa <thg.costa@icloud.com>
2023-02-16 19:34:38 -0300

    commit 26/02 ainda nos testes

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,31 +1,85 @@
 from django.test import TestCase
-from django.urls import reverse, resolve
+from django.urls import resolve, reverse
+
 from recipes import views
+from recipes.models import Category, Recipe, User
 
 
 class RecipeURLsTest(TestCase):
     def test_recipe_home_url_is_correct(self):
         url = reverse('recipes:home')
         self.assertEqual(url, '/')
-        
+
     def test_recipe_category_url_is_correct(self):
         url = reverse('recipes:category', kwargs={'category_id': 1})
         self.assertEqual(url, '/recipes/category/1/')
-        
+
     def test_recipe_detail_url_is_correct(self):
         url = reverse('recipes:recipe', kwargs={'id': 1})
         self.assertEqual(url, '/recipes/1/')
-        
-        
+
+
 class RecipeViewsTest(TestCase):
     def test_recipe_home_views_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
-        
+
+    def test_recipe_home_view_returns_status_code_200_OK(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_view_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response,
+                                'recipes/pages/home.html')
+
+    def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn(
+            'No recipes found',
+            response.content.decode('utf-8'))
+
+    def test_recipe_home_template_loads_recipes(self):
+        category = Category.objects.create(name='Category')
+        author = User.objects.create_user(
+            first_name='user',
+            last_name='nome',
+            username='username',
+            password='123456',
+            email='email@email.com',
+        )
+        recipe = Recipe.objects.create(
+            category = category,
+            author= author,
+            title = 'Title',
+            description = 'Descriptio',
+            slug = 'slug',
+            preparation_time = 10,
+            preparation_time_unit = 'Unit Time',
+            servings = 5,
+            servings_unit = 'Unit Serving',
+            preparation_steps = 'Steps',
+            preparation_steps_is_html =False,
+            is_published = True,
+        )
+        response = self.client.get(reverse('recipes:home'))
+
     def test_recipe_category_views_function_is_correct(self):
         view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
         self.assertIs(view.func, views.category)
-        
-    def test_recipe_recipe_views_function_is_correct(self):
-        view = resolve(reverse('recipes:recipe', kwargs={'id':1}))
+
+    def test_category_view_404_error(self):
+        response = self.client.get(
+            reverse('recipes:category', kwargs={'category_id': 1000})
+        )
+        self.assertEqual(response.status_code, 404)
+
+    def test_recipe_detail_view_404_error(self):
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': 1000})
+        )
+        self.assertEqual(response.status_code, 404)
+
+    def test_recipe_detail_views_function_is_correct(self):
+        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
         self.assertIs(view.func, views.recipe)
